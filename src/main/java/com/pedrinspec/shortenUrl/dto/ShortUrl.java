6cdaf98c0720bd254677cf32b87f1312e070b5f7
pedrin <pedrinho.top07@gmail.com>
2025-06-11 00:35:12 -0300

    fixing database

--- src/main/java/com/pedrinspec/shortenUrl/dto/ShortUrl.java
+++ src/main/java/com/pedrinspec/shortenUrl/dto/ShortUrl.java
@@ -1,26 +1,32 @@
 package com.pedrinspec.shortenUrl.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "shorten_url")
 @Getter
 @Setter
 public class ShortUrl {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
     private Long id;
 
+    @Column(name = "original_url")
     private String originalUrl;
+
+    @Column(name = "shortCode")
     private String shortCode;
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "access_count")
     private Long accessCount;
 
 }
